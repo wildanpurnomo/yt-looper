ff4d6314ec1fc292eda8c52e62d3804169abc137
Wildan F Purnomo <wildanfajarpurnomo@gmail.com>
2021-09-21 16:41:35 +0700

    wait

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueVideoPlayer from "vue-video-player";
+import "video.js/dist/video-js.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueVideoPlayer);
 
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
