537e1655dbba8513634a6c5283e78489c10d890d
Wildan F Purnomo <wildanfajarpurnomo@gmail.com>
2021-09-23 16:17:31 +0700

    this works for dummy video, now to link with yt

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,97 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <v-container fill-height>
+        <v-row align="center">
+          <v-col cols="12" class="d-flex justify-center">
+            <video-player
+              ref="videoPlayer"
+              class="video-player-box"
+              :options="playerOptions"
+              :playsinline="true"
+              @timeupdate="onVideoTimeUpdate($event)"
+              @play="onVideoStartsPlay($event)"
+            >
+            </video-player>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              v-model="ytUrl"
+              label="yt link"
+              hint="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+              persistent-hint
+              outlined
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              v-model="startTime"
+              label="start time in second"
+              hint="120 for 2:00"
+              persistent-hint
+              outlined
+            >
+            </v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              v-model="endTime"
+              label="end time in second"
+              hint="120 for 2:00"
+              persistent-hint
+              outlined
+            >
+            </v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
+  name: "App",
 
   data: () => ({
-    //
+    jobId: -1,
+    isFirstTime: true,
+    ytUrl: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    startTime: 30,
+    endTime: 40,
+    playerOptions: {
+      width: "500px",
+      height: "300px",
+      sources: [
+        {
+          type: "video/mp4",
+          src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm",
+        },
+      ],
+    },
   }),
+
+  computed: {
+    videoPlayer() {
+      return this.$refs.videoPlayer.player;
+    },
+  },
+
+  methods: {
+    onVideoTimeUpdate(player) {
+      //console.log(`${player.cache_.currentTime} / ${player.cache_.duration}`);
+      if (player.cache_.currentTime >= this.endTime) {
+        player.pause();
+        player.play();
+      }
+    },
+    onVideoStartsPlay(player) {
+      player.currentTime(this.startTime);
+    },
+  },
+
+  beforeDestroy() {
+    clearInterval(this.jobId);
+  },
 };
 </script>
