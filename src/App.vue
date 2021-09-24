1c73d6ebf8e864089d97134bbd2b558fa32df1b5
Wildan F Purnomo <wildanfajarpurnomo@gmail.com>
2021-09-24 09:10:53 +0700

    haha done basic feature tho Ill improve later Im tired

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,7 @@
       <v-container fill-height>
         <v-row align="center">
           <v-col cols="12" class="d-flex justify-center">
-            <video-player
-              ref="videoPlayer"
-              class="video-player-box"
-              :options="playerOptions"
-              :playsinline="true"
-              @timeupdate="onVideoTimeUpdate($event)"
-              @play="onVideoStartsPlay($event)"
-            >
-            </video-player>
+            <video controls ref="videoPlayer" class="video-js" />
           </v-col>
           <v-col cols="4">
             <v-text-field
@@ -43,6 +35,9 @@
             >
             </v-text-field>
           </v-col>
+          <v-col cols="12" class="d-flex justify-center">
+            <v-btn color="primary" @click="instantiatePlayer">Lezgo</v-btn>
+          </v-col>
         </v-row>
       </v-container>
     </v-main>
@@ -50,48 +45,66 @@
 </template>
 
 <script>
+import "videojs-youtube";
+import "video.js/dist/video-js.css";
+import videojs from "video.js";
 export default {
   name: "App",
 
   data: () => ({
-    jobId: -1,
     isFirstTime: true,
+    player: null,
     ytUrl: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     startTime: 30,
     endTime: 40,
     playerOptions: {
       width: "500px",
       height: "300px",
-      sources: [
-        {
-          type: "video/mp4",
-          src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm",
-        },
-      ],
+      techOrder: ["youtube"],
+      sources: [],
     },
   }),
 
-  computed: {
-    videoPlayer() {
-      return this.$refs.videoPlayer.player;
-    },
-  },
-
   methods: {
-    onVideoTimeUpdate(player) {
+    onVideoTimeUpdate() {
       //console.log(`${player.cache_.currentTime} / ${player.cache_.duration}`);
-      if (player.cache_.currentTime >= this.endTime) {
-        player.pause();
-        player.play();
+      if (this.player.cache_.currentTime >= this.endTime) {
+        this.isFirstTime = true;
+        this.rewind();
       }
     },
-    onVideoStartsPlay(player) {
-      player.currentTime(this.startTime);
+    rewind() {
+      if (this.isFirstTime) {
+        this.isFirstTime = false;
+        this.player.currentTime(this.startTime);
+      }
+    },
+    instantiatePlayer() {
+      this.disposePlayerIfExists();
+      this.$set(
+        (this.playerOptions.sources = [
+          {
+            type: "video/youtube",
+            src: this.ytUrl,
+          },
+        ])
+      );
+      this.player = videojs(this.$refs.videoPlayer, this.playerOptions);
+      this.player.on("timeupdate", this.onVideoTimeUpdate);
+      this.player.on("play", this.rewind);
+      this.player.on("ready", () => {
+        this.player.play();
+      });
+    },
+    disposePlayerIfExists() {
+      if (this.player) {
+        this.player.dispose();
+      }
     },
   },
 
   beforeDestroy() {
-    clearInterval(this.jobId);
+    this.disposePlayerIfExists();
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
-import VueVideoPlayer from "vue-video-player";
-import "video.js/dist/video-js.css";
 
 Vue.config.productionTip = false;
-
-Vue.use(VueVideoPlayer);
 
 new Vue({
   vuetify,
